77d056c3f65900f6733f40ab5c6e36ad44f1cd1d
YZhLu <47222845+YZhLu@users.noreply.github.com>
2024-06-01 12:40:41 -0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,9 +35,25 @@
 		padding: 'pl-20',
 		rounded: 'rounded-none'
 	};
+
+	const drawerSettingsMobile: DrawerSettings = {
+		id: 'example-3',
+		position: 'top',
+		// Provide your property overrides:
+		bgDrawer: 'bg-surface-50-900-token',
+		//  bgBackdrop: 'bg-gradient-to-tr from-indigo-500/50 via-purple-500/50 to-pink-500/50',
+		bgBackdrop: 'bg-transparent',
+		width: 'w-screen',
+		padding: 'pt-14',
+		rounded: 'rounded-none'
+	};
 	const drawerStore = getDrawerStore();
 	function drawerOpen(): void {
 		drawerStore.open(drawerSettings);
+	}
+
+	function drawerOpenMobile(): void {
+		drawerStore.open(drawerSettingsMobile);
 	}
 </script>
 
@@ -109,41 +125,43 @@
 	</AppRail>
 
 	<div class="grid place-content-center place-items-center">
-		<div class="md:hidden flex justify-center items-center">
-			<span class="flex justify-center"><Avatar src="/timbu.png" width="w-11" /></span>
+		<div class="md:hidden flex justify-between items-center w-screen py-1 px-4">
+			<span class="flex justify-center"><Avatar src="/timbu.png" width="w-8" /></span>
 			<!-- <AppRailAnchor href="https://github.com/" target="_blank" title="GitHub"> -->
-			<span class="flex justify-center">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="3rem"
-					height="3rem"
-					viewBox="0 0 24 24"
-					{...$$props}
-				>
-					<path
-						fill="currentColor"
-						d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"
-					/>
-				</svg>
-			</span>
-			<!-- </AppRailAnchor> -->
-
-			<button on:click={drawerOpen}>
-				<span class="flex justify-center items-center md:mb-4">
+			<div class="flex gap-2">
+				<span class="flex justify-center items-center">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
-						width="3rem"
-						height="3rem"
+						width="2rem"
+						height="2rem"
 						viewBox="0 0 24 24"
 						{...$$props}
 					>
 						<path
 							fill="currentColor"
-							d="M20 11H4c-.6 0-1 .4-1 1s.4 1 1 1h16c.6 0 1-.4 1-1s-.4-1-1-1M4 8h16c.6 0 1-.4 1-1s-.4-1-1-1H4c-.6 0-1 .4-1 1s.4 1 1 1m16 8H4c-.6 0-1 .4-1 1s.4 1 1 1h16c.6 0 1-.4 1-1s-.4-1-1-1"
+							d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"
 						/>
 					</svg>
 				</span>
-			</button>
+				<!-- </AppRailAnchor> -->
+
+				<button on:click={drawerOpenMobile}>
+					<span class="flex justify-center items-center md:mb-4">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="2rem"
+							height="2.75rem"
+							viewBox="0 0 24 24"
+							{...$$props}
+						>
+							<path
+								fill="currentColor"
+								d="M20 11H4c-.6 0-1 .4-1 1s.4 1 1 1h16c.6 0 1-.4 1-1s-.4-1-1-1M4 8h16c.6 0 1-.4 1-1s-.4-1-1-1H4c-.6 0-1 .4-1 1s.4 1 1 1m16 8H4c-.6 0-1 .4-1 1s.4 1 1 1h16c.6 0 1-.4 1-1s-.4-1-1-1"
+							/>
+						</svg>
+					</span>
+				</button>
+			</div>
 		</div>
 		<!-- <span class="badge variant-soft">Tile Value: {currentTile}</span> -->
 		<div class="w-screen md:w-[calc(100vw_-_80px)]">
